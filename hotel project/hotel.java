693be1bcb05caf2bd1840199ff4aeec8326a1b7d
Brian Dong <24dongb@308-09.cls-edu.charlottelatin.org>
2022-09-09 09:52:46 -0400

    sep.9.hotel.fr

--- hotel project/hotel.java
+++ hotel project/hotel.java
@@ -1,19 +1,36 @@
 public class hotel {
     int roomNum;
+    int roomSize;
     person guestOne;
     person guestTwo;
     
-    hotel(int num){
+    hotel(int num, int capacity){
         roomNum = num;
-
+        roomSize = capacity;
     }
 
-    void checkIn(person newGuest){
-        guestOne = newGuest;
+    void checkIn(person guest){
+        if(guestOne == null){
+            guestOne = guest;
+        }
+        else if(guestTwo == null){
+            guestTwo = guest;
+        }
+        else{
+            System.out.println("This room can only hold 2 people.");
+        }
     }
 
-    void checkOut(){
-
+    void checkOut(person guest){
+        if(guestOne == guest){
+            guestOne = null;
+        }
+        else if(guestTwo == guest){
+            guestTwo = null;
+        }
+        else{
+            System.out.println("There's no one to check out from this room.");
+        }
     }
 
 }
